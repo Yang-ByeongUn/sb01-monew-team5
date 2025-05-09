a481fc6b5d0a5d4e9f63ea49931f71ad2202a61f
Yang-ByeongUn <143311964+Yang-ByeongUn@users.noreply.github.com>
2025-05-09 19:21:20 +0900

    Update ArticleViewServiceInterface.java

--- src/main/java/com/example/part35teammonew/domain/articleView/service/ArticleViewServiceInterface.java
+++ src/main/java/com/example/part35teammonew/domain/articleView/service/ArticleViewServiceInterface.java
@@ -1,7 +1,7 @@
 package com.example.part35teammonew.domain.articleView.service;
 
 import com.example.part35teammonew.domain.article.entity.Direction;
-import com.example.part35teammonew.domain.articleView.Dto.ArticleViewDto;
+import com.example.part35teammonew.domain.articleView.dto.ArticleViewDto;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.domain.Pageable;
@@ -11,5 +11,8 @@ public interface ArticleViewServiceInterface {
   ArticleViewDto createArticleView(UUID articleId);//기사 만들어 질때 해결
 
   boolean addReadUser(UUID articleId, UUID userId);//유저가 기사 읽을때, //뭔가 문제 생김
+
+  List<UUID> getSortByViewCountPageNation(Long cursor, Pageable pageable,
+      String direction);//조회수 기반 페이지네이션
 
 }
